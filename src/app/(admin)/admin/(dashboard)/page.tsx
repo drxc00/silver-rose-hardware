8c80e6f95e3c42f8de09997e81c378d170d81b93
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-02-15 22:53:17 +0800

    feat: enhance admin dashboard layout and improve AdminHeader component for better navigation

--- src/app/(admin)/admin/(dashboard)/page.tsx
+++ src/app/(admin)/admin/(dashboard)/page.tsx
@@ -1,25 +1,25 @@
-import authCache from "@/lib/auth-cache"
+import { AdminHeader } from "@/components/admin/admin-header";
+import authCache from "@/lib/auth-cache";
 import { UserRole } from "@/lib/constants";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 export default async function AdminPage() {
-    // Check if there is a user logged in
-    const session = await authCache();
-    if (!session) redirect('/admin/login');
+  // Check if there is a user logged in
+  const session = await authCache();
+  if (!session) redirect("/admin/login");
 
-    // Check if the user is an admin
-    if (session?.user?.role !== UserRole.ADMIN) redirect('/');
+  // Check if the user is an admin
+  if (session?.user?.role !== UserRole.ADMIN) redirect("/");
 
-    return (
-        <div className="flex flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <div className="flex flex-col gap-6">
-                    <div className="flex flex-col gap-6">
-                        <div className="flex flex-col items-center gap-2">
-                            <h1 className="text-xl font-bold">Admin Panel</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="min-h-screen bg-muted w-vh">
+      <AdminHeader currentPage="Home" />
+      <main className="p-6 flex flex-col gap-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold">Dashboard</h1>
         </div>
-    )
-}   
+        <div></div>
+      </main>
+    </div>
+  );
+}
--- src/components/admin/admin-header.tsx
+++ src/components/admin/admin-header.tsx
@@ -1,47 +1,51 @@
 import * as React from "react";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
 } from "../ui/breadcrumb";
 import { Separator } from "../ui/separator";
 import { SidebarTrigger } from "../ui/sidebar";
 
 interface AdminHeaderProps {
-    currentPage: string;
-    crumbItems?: { name: string; href: string }[];
+  currentPage?: string;
+  crumbItems?: { name: string; href: string }[];
 }
 
 export function AdminHeader({ currentPage, crumbItems }: AdminHeaderProps) {
-    return (
-        <header className="sticky top-0 w-full flex bg-sidebar items-center border-b p-3 z-50">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mx-2" />
-            <div>
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="/admin">Dashboard</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbSeparator />
-                        {crumbItems && crumbItems.map((item) => (
-                            <React.Fragment key={item.name}>
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink href={item.href}>{item.name}</BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator/>
-                            </React.Fragment>
-                        ))}
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>{currentPage}</BreadcrumbPage>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </div>
-        </header>
-    )
-
+  return (
+    <header className="sticky top-0 w-full flex bg-sidebar items-center border-b p-3 z-50">
+      <SidebarTrigger />
+      <Separator orientation="vertical" className="mx-2" />
+      <div>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/admin">Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            {crumbItems &&
+              crumbItems.map((item) => (
+                <React.Fragment key={item.name}>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href={item.href}>
+                      {item.name}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                </React.Fragment>
+              ))}
+            {currentPage && (
+              <BreadcrumbItem>
+                <BreadcrumbPage>{currentPage}</BreadcrumbPage>
+              </BreadcrumbItem>
+            )}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </header>
+  );
 }
