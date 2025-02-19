63237e69243ea7b94db4057054441a66d3224930
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-02-19 23:03:00 +0800

    feat: add suppressHydrationWarning for ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} antialiased`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <AuthProvider>
